841274e88ad0f9769c3e72d42d86af4f8a9ea01f
crystisor <crystisor4@gmail.com>
2024-04-29 21:07:59 +0300

    Am mai facut cateva modificari, minore, la clasele de baza.

--- src/Library.java
+++ src/Library.java
@@ -1,5 +1,10 @@
 public class Library
 {
+    private int bookCount;
+    public Book buyBook(Book book)
+    {
+        return book;
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
