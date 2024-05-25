68f5f8d6367bef1f2675218475c18e383997620b
sxergiu <sergiu.varga@student.upt.ro>
2024-05-25 12:56:52 +0300

    Card validation + style changes

--- src/main/java/org/project/Forms/CardForm.java
+++ src/main/java/org/project/Forms/CardForm.java
@@ -2,37 +2,55 @@ package org.project.Forms;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CardForm extends JDialog{
-    private JTextField tfNumber;
-    private JTextField tfOwner;
-    private JTextField tfExp;
-    private JTextField tfCVV;
     private JLabel cardLabel;
     private JLabel codeLabel;
     private JLabel nameLabel;
     private JLabel expLabel;
     private JLabel secLabel;
-    private JTextField tfAmount;
     private JLabel amountLabel;
     private JPanel cardPanel;
+    private JButton btnConfirm;
+    private JButton btnCancel;
+    private JPasswordField pfCVV;
+    private JTextField tfOwner;
+    private JTextField tfNumber;
+    private JTextField tfExp;
+    private JTextField tfAmount;
 
 
     public CardForm(JDialog parent) {
         super(parent);
         setTitle("Card Form");
         setContentPane(cardPanel);
-        setMinimumSize(new Dimension(650,200));
+        setMinimumSize(new Dimension(650,250));
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         setContentPane(cardPanel);
 
+        btnConfirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
         setVisible(true);
     }
 
     public static void main(String[] args) {
         CardForm cf = new CardForm(null);
     }
+
 }
